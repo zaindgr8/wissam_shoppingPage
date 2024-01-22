490168062ad5f09907260ad553961e1ff9cbea7e
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2024-01-22 19:00:18 +0400

    22 jan 24

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 // import GridLayout from "@/components/GridLayout";
+"use client"
 import Layout from "@/components/Layout";
 import Regions from "@/components/Regions";
 import FeaturesProperties from "@/components/feature-properties";
@@ -9,6 +10,10 @@ import Dropdown from "@/components/form-control-two";
 import Testimonial from "../data/testimonial.json";
 
 export default async function Home() {
+  const handleChatButtonClick = () => {
+    window.location.href =
+      "mailto:wissambywissam@gmail.com?subject=Inquiry&body=Drop%20us%20a%20message";
+  };
   return (
     <Layout>
       <main>
@@ -19,7 +24,6 @@ export default async function Home() {
               alt=""
               className="h-100 object-fit-cover position-absolute w-100 top-0 left-0"
             />
-           
           </div>
           {/* Start Top Regions Section */}
           <div className="bg-white py-5 border-bottom">
@@ -30,7 +34,7 @@ export default async function Home() {
               <div className="text-center mt-5" data-aos="fade-up">
                 {/* Start Button */}
                 <button className="btn text-white bg-gray-900  hover:bg-gray-700 btn-lg d-inline-flex hstack gap-2">
-                  <span>Contact Now</span>
+                  <span onClick={handleChatButtonClick}>Contact Now</span>
                   <span className="vr" />
                   <i className="fa-arrow-right fa-solid fs-14" />
                 </button>
